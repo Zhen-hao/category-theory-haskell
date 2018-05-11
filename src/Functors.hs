60ef435e2bcab9890f6cfc257bc82224dcd41c99
Zhen-hao <zhenhaolee@gmail.com>
2018-05-11 08:48:46 +0200

    added src/NaturalTransformations.hs

--- src/Functors.hs
+++ src/Functors.hs
@@ -1,9 +1,47 @@
-module Functor where
+module Functors where
 import Data.Bifunctor
+import Data.Functor.Const
+import Data.Functor.Identity
+import Data.Functor.Contravariant
 
-data K2 c a b = K2 c
-data Fst a b = Fst a
-data Snd a b = Snd b
+newtype Reader e a = Reader (e -> a)
+
+instance Functor (Reader e) where
+    fmap f (Reader g) = Reader (f . g)
+
+newtype Op r a = Op (a -> r)
+instance Contravariant (Op r) where
+    contramap f (Op g) = Op (g . f)
+
+-- Challenge 1
+data Pair a b = Pair a b
+
+instance Bifunctor Pair where
+  bimap f g (Pair x y) = Pair (f x) (g y)
+
+-- Challenge 2
+type Maybe' a = Either (Const () a) (Identity a)
+
+mapF ::  Maybe' a -> Maybe a
+mapF (Left (Const ())) = Nothing
+mapF (Right y) = Just (runIdentity y)
+
+mapB :: Maybe a -> Maybe' a
+mapB Nothing = Left (Const ())
+mapB (Just x) = Right (Identity x)
+
+-- Challenge 3
+data PreList a b = Nil | Cons a b
+
+instance Bifunctor PreList where
+  bimap f g Nil = Nil
+  bimap f g (Cons a b) = Cons (f a) (g b)
+
+
+-- Challenge 4
+newtype K2 c a b = K2 c
+newtype Fst a b = Fst a
+newtype Snd a b = Snd b
 
 
 instance Bifunctor (K2 c) where
@@ -14,11 +52,3 @@ instance Bifunctor Fst where
 
 instance Bifunctor Snd where
   bimap f g (Snd b) = Snd (g b)
-
-
-
-data PreList a b = Nil | Cons a b
-
-instance Bifunctor PreList where
-  bimap f g Nil = Nil
-  bimap f g (Cons a b) = Cons (f a) (g b)
